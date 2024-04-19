98b7270873d0569ddb0a5b83f86c3f8932833158
Muntasir <muntasirsunny05@gmail.com>
2024-04-19 17:46:03 +0600

    building and populating the bookmarks

--- script.js
+++ script.js
@@ -41,6 +41,39 @@ function validate(nameValue, urlValue) {
     return true;
   }
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+    // Build items
+    bookmarks.forEach((bookmark) => {
+        const {name, url} = bookmark;
+        // console.log(name, url);
+        // Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // Close Icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas', 'fa-times');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url})`);
+        // Favicon / Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        // Favicon
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt', 'Favicon');
+        // Link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        // Append to bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.append(item);
+    })
+}
+
 // Fetch Bookmarks
 function fetchBookmarks() {
     // Get bookmarks from localStorage if available also converting them from json string to js object using parse
@@ -58,8 +91,11 @@ function fetchBookmarks() {
         //saving default bookmarks into the local storage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
-    console.log(bookmarks);
+    // console.log(bookmarks);
+    buildBookmarks();
 }
+
+
 
 // Handle Data from Form
 function storeBookmark(e) {
